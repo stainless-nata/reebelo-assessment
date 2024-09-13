33a8753063e60db6ae2208fc110a69db33f7c79f
stainless-nata <workingkeo@gmail.com>
2024-09-13 20:18:51 +0200

    feat: create database schema and migration for order table

---
+++ order-service/src/db/data-source.ts
@@ -0,0 +1,22 @@
+import { DataSource } from 'typeorm';
+import {
+  DB,
+  DATABASE_HOST,
+  DATABASE_NAME,
+  DATABASE_PASSWORD,
+  DATABASE_PORT,
+  DATABASE_USER,
+} from 'src/utils/env';
+
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: DATABASE_HOST,
+  port: parseInt(DATABASE_PORT),
+  username: DATABASE_USER,
+  password: DATABASE_PASSWORD,
+  database: DATABASE_NAME,
+  entities: [__dirname + '/**/*.entity.{ts,js}'],
+  migrations: [__dirname + '/migration/*.{ts,js}'],
+  synchronize: false,
+  logging: true,
+});
---
+++ order-service/src/entity/order.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn, Double, IntegerType } from 'typeorm';
+
+@Entity()
+export class Order {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  customerId: string;
+
+  @Column('json')
+  inventories: { inventoryId: string; quantity: IntegerType; price: Double; }[];
+
+  @Column()
+  status: string;
+
+  @Column({ nullable: true })
+  trackingNumber?: string;
+
+  @Column({ nullable: true })
+  trackingCompany?: string;
+}
